866847d620545e43263eb3a205a097bfb1fddf75
BhelPuriPanda <boreover582@gmail.com>
2026-01-17 16:55:37 +0530

    Version history of JD implememted

--- backend-node/src/routes/jdRoutes.js
+++ backend-node/src/routes/jdRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { generateJobDescriptionAB } from "../controllers/jdController.js";
+import { generateJobDescriptionAB , getJD , saveEditedJD , switchJDVersion } from "../controllers/jdController.js";
 
 const router = express.Router();
 
 // POST /api/job-description
 router.post("/generate-ab", generateJobDescriptionAB);
+router.get("/:jdId", getJD);
+router.patch("/:jdId/save-version", saveEditedJD);
+router.patch("/:jdId/switch-version", switchJDVersion);
 
 export default router;
